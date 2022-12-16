830a19831a539b8f6b5b18cf099730ef828072c8
JTh <justathought1408@gmail.com>
2022-12-16 11:41:33 +0300

    django blog 1

--- app/models.py
+++ app/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class FM(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Заголовок')
-    desc = models.CharField(max_length=1200, verbose_name='Описание')
-    image1 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Фото 1')
-    image2 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Фото 2')
-    image3 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Фото 3')
-    image4 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Фото 4')
-    image5 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Фото 5')
-    slug = models.SlugField(unique=True, verbose_name='Адрес записи')
-    is_pub = models.BooleanField(default=False, verbose_name='Опубликовано')
+    title = models.CharField(max_length=100, verbose_name='Title')
+    desc = models.CharField(max_length=1200, verbose_name='Description')
+    image1 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Photo 1')
+    image2 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Photo 2')
+    image3 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Photo 3')
+    image4 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Photo 4')
+    image5 = models.ImageField(upload_to='media/%Y/%m/%d', blank=True, verbose_name='Photo 5')
+    slug = models.SlugField(unique=True, verbose_name='URL')
+    is_pub = models.BooleanField(default=False, verbose_name='Published')
 
     def __str__(self):
         return self.title 
 
     class Meta:
-        verbose_name_plural = 'Модель'
+        verbose_name_plural = 'Model'
