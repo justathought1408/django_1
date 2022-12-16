c1d0bfc5ab95fc09685956dab913382b80540460
JTh <justathought1408@gmail.com>
2022-12-16 14:37:53 +0300

    django blog 1

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,7 @@ from .models import FM
 
 class FMAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'desc', 'image1', 'image2', 'image3', 'image4', 'image5', 'slug', 'is_pub'] 
+    search_fields = ['id', 'title', 'desc', 'image1', 'image2', 'image3', 'image4', 'image5', 'slug', 'is_pub'] 
     list_editable = ['title', 'desc', 'image1', 'image2', 'image3', 'image4', 'image5', 'slug', 'is_pub']
     actions = ['set_pub', 'delete_desc', 'delete_photos']
 
